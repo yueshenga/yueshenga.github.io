8839a4dff928b1173c626fec93e4d2aeefe3de71
yueshenga <102970763+yueshenga@users.noreply.github.com>
2022-11-04 08:05:38 +0000

    github action update Updated By Github Actions

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,1 +1,1 @@
-var posts=["2022/11/01/hello-world/","2022/10/25/技术/技术/","2022/10/25/生活/表白/"];function toRandomPost(){window.open("/"+posts[Math.floor(Math.random()*posts.length)],"_self")}
+var posts=["2022/11/04/hello-world/","2022/10/25/技术/技术/","2022/10/25/生活/表白/"];function toRandomPost(){window.open("/"+posts[Math.floor(Math.random()*posts.length)],"_self")}
